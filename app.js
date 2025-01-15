2ac1e0e55fc7196f287f0cfb4ebbd8212bc53a08
wahyurahmana <tn.wahyurahmana@gmail.com>
2025-01-15 13:32:53 +0800

    fix whatswebjs

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const {nanoid} = require('nanoid')
 
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args : ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions']
+  },
   webVersionCache: {
     type: 'remote',
     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
